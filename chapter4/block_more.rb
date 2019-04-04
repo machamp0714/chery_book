e62fa3c02e5e183056a40cd91d2e4e0f72df526f
25 macamp <rateliasudaisuki@gmail.com>
2019-04-04 23:27:23 +0900

    4.8.3

--- chapter4/block_more.rb
+++ chapter4/block_more.rb
@@ -4,3 +4,6 @@ fruites = %w(apple orrange melon)
 fruites.each_with_index { |fruite, i| puts "#{i}: #{fruite}" } 
 p fruites.map.with_index { |fruite, i| "#{i}: #{fruite}" }
 p fruites.delete_if.with_index { |fruite, i| fruite.include?('a') &&  i.odd? }
+
+## 4.8.3 添え字を０以外の数値から開始させる
+fruites.each.with_index(1) { |fruite, i| puts "#{i}: #{fruite}" }

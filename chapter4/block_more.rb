fbfd160e04070139d6054b4f31e122a7348f6c5d
25 macamp <rateliasudaisuki@gmail.com>
2019-04-04 23:46:32 +0900

    4.8.4

--- chapter4/block_more.rb
+++ chapter4/block_more.rb
@@ -6,4 +6,37 @@ p fruites.map.with_index { |fruite, i| "#{i}: #{fruite}" }
 p fruites.delete_if.with_index { |fruite, i| fruite.include?('a') &&  i.odd? }
 
 ## 4.8.3 添え字を０以外の数値から開始させる
-fruites.each.with_index(1) { |fruite, i| puts "#{i}: #{fruite}" }
+fruites.each.with_index(1) { |fruite, i| puts "#{i}: #{fruite}" } # each_with_indexメソッドには引数を渡せない
+
+## 4.8.4 配列がブロック引数に渡される場合
+dimensions = [
+    [10, 20],
+    [30, 40],
+    [50, 60],
+]
+areas = []
+dimensions.each do |dimension|
+    length = dimension[0]
+    width = dimension[1]
+    areas << length * width
+end
+p areas
+
+dimensions.each do |length, width|
+    areas << length * width
+end
+p areas
+
+dimensions.each_with_index do |length, width, i|
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+dimensions.each_with_index do |dimension, i|
+    length = dimension[0]
+    width = dimension[1]
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+dimensions.each_with_index do |(length, width), i|
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end

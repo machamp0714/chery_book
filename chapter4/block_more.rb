82d0852121abc92b36603ed7aefd6331371d16c8
25 macamp <rateliasudaisuki@gmail.com>
2019-04-04 23:58:50 +0900

    4.8.5

--- chapter4/block_more.rb
+++ chapter4/block_more.rb
@@ -40,3 +40,13 @@ end
 dimensions.each_with_index do |(length, width), i|
     puts "length: #{length}, width: #{width}, i: #{i}"
 end
+
+## 4.8.5 ブロックローカル変数
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n; sum| # ブロック内でのみ有効なローカル変数を宣言できる
+    sum = 10
+    sum += n
+    p sum
+end
+p sum

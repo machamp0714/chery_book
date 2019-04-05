b1585f75c965356756707257b8f762a22d36e7c5
25 macamp <rateliasudaisuki@gmail.com>
2019-04-05 22:05:07 +0900

    4.9.2

--- chapter4/each_more.rb
+++ chapter4/each_more.rb
@@ -1,5 +1,14 @@
 # 様々な繰り返し処理
-## timesメソッド
+## 4.9.1 timesメソッド
 sum = 0
-5.times { |n| sum += n }
+5.times { |n| sum += n } # 配列を使わず、単純にn回処理を繰り返したい、という場合はIntegerクラスのtimesメソッドが便利
 p sum
+
+# 4.9.2 uptoとdowntoメソッド
+a = []
+10.upto(14) { |n| a << n } # nからmまで数値を1ずつ増やしながら処理をしたい時に用いる
+p a
+
+d = []
+14.downto(10) { |n| d << n }
+p d

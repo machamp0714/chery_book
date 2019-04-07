92a117f095ba8b8a10bf6bfa15e05214eb549e8f
25 macamp <rateliasudaisuki@gmail.com>
2019-04-07 23:11:28 +0900

    5.2.3

--- chapter5/hash.rb
+++ chapter5/hash.rb
@@ -9,3 +9,13 @@ currencies.each do |key_value|
     value = key_value[1]
     puts "#{key}: #{value}"
 end
+
+## 5.2.3 ハッシュの同値比較、要素数の取得、要素の削除
+a = { x: 1, y: 2, z: 3 }
+b = { x: 1, y: 2, z: 3 }
+p a == b
+p a.size
+a.delete(:x)
+p a
+p currencies.delete(:taiwan) # deleteメソッドで指定したキーがなければ、nilが返る
+p currencies.delete(:taiwan) { |key| "Not found #{key}" } # ブロックを渡すと、キーが見つからない時の戻り値を作成できる

a24fce2a6692f3b2faac21fa0a46e2dc4d822062
25 macamp <rateliasudaisuki@gmail.com>
2019-04-08 23:03:46 +0900

    5.7.3

--- chapter5/symbol_more.rb
+++ chapter5/symbol_more.rb
@@ -4,3 +4,13 @@ p %s!ruby is fun! # !を区切り文字に使う
 p %s(ruby is fun) # ()を区切り文字に使う
 
 p %i(apple orange melon)
+
+## 5.7.3 シンボルと文字列の関係
+string = 'apple'
+symbol = :apple
+p string == symbol
+p string.to_sym == symbol
+p string + symbol.to_s
+
+p 'apple'.respond_to?('include?')
+p 'apple'.respond_to?(:include?)

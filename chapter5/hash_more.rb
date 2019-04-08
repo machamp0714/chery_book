da04b88300688d9f5fd4254c266d747f5b2df34f
25 macamp <rateliasudaisuki@gmail.com>
2019-04-08 22:21:22 +0900

    5.6.4

--- chapter5/hash_more.rb
+++ chapter5/hash_more.rb
@@ -4,3 +4,20 @@ currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
 p currencies.keys
 p currencies.values
 p currencies.has_key?(:italy) # has_key?/key?/include?/member?は全てエイリアスメソッド
+
+## 5.6.3 ハッシュを使った擬似キーワード引数
+def buy_burger(menu, options = {})
+    drink = options[:drink]
+    potato = options[:potato]
+    if drink && potato
+        puts "#{menu}セット: サイドメニューはdrinkとpotato"
+    elsif drink
+        puts "#{menu}セット: サイドメニューはdrink"
+    else
+        puts "#{menu}セット: サイドメニューはpotato"
+    end
+end
+buy_burger('cheese_burger', drink: true, potato: true)
+buy_burger('cheese_burger', drink: true)
+buy_burger('cheese_burger', potato: true)
+buy_burger('cheese_burger', drink: true, potato: true, salada: true) # 擬似キーワード引数は単なるハッシュであるため、どんなキーを渡してもエラーにならない

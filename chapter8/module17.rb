bff4842eaaf2fcc7ee6aa5188cd54bc60dacfd1d
25 macamp <rateliasudaisuki@gmail.com>
2019-04-13 23:30:36 +0900

    8.9.4

---
+++ chapter8/module17.rb
@@ -0,0 +1,19 @@
+# prependメソッドで既存メソッドを置き換える
+class Product
+  def name
+    'A great film'
+  end
+end
+
+module NameDecorator
+  def name
+    "<<#{super}>>"
+  end
+end
+
+class Product
+  prepend NameDecorator
+end
+
+product = Product.new
+p product.name

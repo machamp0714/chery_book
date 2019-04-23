eab38444f892ac6f4e23ab3541f3bbbc336522d6
25 macamp <rateliasudaisuki@gmail.com>
2019-04-23 22:47:25 +0900

    ex 2106

---
+++ ruby_ex/ex2106.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'dbi'
+
+dbh = DBI.connect('DBI:SQLite3:student01.db')
+
+dbh.select_all('select * from students') do |row|
+  puts "row = #{row}"
+  puts "name = #{row[0]}"
+  puts "age = #{row[1]}"
+end
+
+dbh.disconnect

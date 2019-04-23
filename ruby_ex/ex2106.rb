5115d35c4d0929712e3ec719d887acb787862093
25 macamp <rateliasudaisuki@gmail.com>
2019-04-23 22:58:37 +0900

    ex 2107

--- ruby_ex/ex2106.rb
+++ ruby_ex/ex2106.rb
@@ -3,7 +3,7 @@ require 'dbi'
 
 dbh = DBI.connect('DBI:SQLite3:student01.db')
 
-dbh.select_all('select * from students') do |row|
+dbh.select_all('select * from students where age > 50') do |row|
   puts "row = #{row}"
   puts "name = #{row[0]}"
   puts "age = #{row[1]}"

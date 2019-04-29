259a19489f158f013a187678bd976b1c135651c8
25 macamp <rateliasudaisuki@gmail.com>
2019-04-29 19:16:31 +0900

    viewの作り方

--- mysql/chapter5.sql
+++ mysql/chapter5.sql
@@ -1,1 +1,19 @@
 -- ビュー
+/*
+CREATE VIEW ShohinSum (shohin_bunrui, cnt_shohin)
+AS 
+SELECT shohin_bunrui, COUNT(*)
+FROM Shohin
+GROUP BY shohin_bunrui;
+*/
+
+-- ビューを使う
+-- SELECT * FROM ShohinSum;
+
+-- 多段ビュー
+-- CREATE VIEW ShohinSumJim (shohin_bunrui, cnt_shohin)
+-- AS
+-- SELECT shohin_bunrui, cnt_shohin
+-- FROM ShohinSum
+-- WHERE shohin_bunrui = '事務用品';
+

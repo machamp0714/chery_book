b8a2f6616149b5da46dbc3a6a8793f23fd8bb5b2
25 macamp <rateliasudaisuki@gmail.com>
2019-05-14 22:20:30 +0900

    BlackJack 中断

--- training11/lib/black_jack.rb
+++ training11/lib/black_jack.rb
@@ -1,5 +1,28 @@
-require './player.rb'
+require './player'
+require './deck'
 
-player = Player.new
-player.draw_a_card
-p player.hand
+class BlackJack
+  def first_turn(player)
+    2.times do
+      player.draw_a_card
+      puts player.present_a_card
+    end
+  end
+
+  def play
+    player = Player.new
+
+    puts "☆★☆★☆★☆★☆★ ブラックジャックへようこそ！ ☆★☆★☆★☆★☆★"
+    puts "ゲームを開始します。"
+
+    first_turn(player)
+    puts player.calc_score
+
+    puts "カードを引きますか？引く場合はYを、引かない場合はNを入力してください"
+    answer = gets.chomp
+    
+  end
+end
+
+game = BlackJack.new
+game.play
--- training11/lib/player.rb
+++ training11/lib/player.rb
@@ -5,12 +5,34 @@ class Player
 
   def initialize
     @hand = []
+    @score = 0
   end
 
   def draw_a_card
     card = Deck::DECK.sample
     remove_a_card(card)
     @hand << card
+  end
+
+  def present_a_card
+    "あなたの引いたカードは#{hand.last.suit}の#{hand.last.number}です。"
+  end
+
+  def calc_score
+    hand.each do |card|
+      if card.number == 'A'
+        @score += 1
+      elsif card.number == 'J'
+        @score += 10
+      elsif card.number == 'Q'
+        @score += 10
+      elsif card.number == 'K'
+        @score += 10
+      else
+        @score += card.number
+      end
+    end
+    "あなたの現在の得点は#{@score}です。"
   end
 
   private

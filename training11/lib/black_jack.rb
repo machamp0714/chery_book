81558ab80a422ce81bf3dab4620f13381f115d0c
25 macamp <rateliasudaisuki@gmail.com>
2019-05-14 20:59:04 +0900

    draw_a_cardメソッド追加

---
+++ training11/lib/black_jack.rb
@@ -0,0 +1,5 @@
+require './player.rb'
+
+player = Player.new
+player.draw_a_card
+p player.hand
--- training11/lib/player.rb
+++ training11/lib/player.rb
@@ -1,7 +1,20 @@
+require './deck'
+  
 class Player
+  attr_reader :hand
+
   def initialize
     @hand = []
   end
 
-  
+  def draw_a_card
+    card = Deck::DECK.sample
+    remove_a_card(card)
+    @hand << card
+  end
+
+  private
+    def remove_a_card(card)
+      Deck::DECK.delete(card)
+    end
 end
